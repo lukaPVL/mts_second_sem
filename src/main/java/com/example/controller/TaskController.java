66b17729e1a8ce82d542f0a2f2f59434520842a7
Luka Pavlov <pavlovld2007@gmail.com>
2026-02-26 21:55:34 +0300

    creating RequestScopedBean and PrototypeScopedBean

--- src/main/java/com/example/controller/TaskController.java
+++ src/main/java/com/example/controller/TaskController.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
 import com.example.model.Task;
+import com.example.scope.PrototypeScopedBean;
+import com.example.scope.RequestScopedBean;
 import org.springframework.web.bind.annotation.*;
 import com.example.service.TaskService;
 
@@ -10,13 +12,28 @@ import java.util.List;
 @RequestMapping("/api/tasks")
 public class TaskController {
 	private final TaskService taskService;
+	private final RequestScopedBean requestScopedBean;
+	private final PrototypeScopedBean prototypeScopedBean;
 
-	public TaskController(TaskService taskService) {
+	public TaskController(TaskService taskService,
+												RequestScopedBean requestScopedBean,
+												PrototypeScopedBean prototypeScopedBean) {
 		this.taskService = taskService;
+		this.requestScopedBean = requestScopedBean;
+		this.prototypeScopedBean = prototypeScopedBean;
 	}
 
 	@GetMapping
 	public List<Task> getAllTasks() {
+
+		System.out.println("REQUEST SCOPED BEAN");
+		System.out.println("Request ID: " + requestScopedBean.getRequestId());
+		System.out.println("Start time: " + requestScopedBean.getStartTime());
+
+		System.out.println("PROTOTYPE SCOPED BEAN");
+		System.out.println("Instance ID: " + prototypeScopedBean.getInstanceId());
+		System.out.println("Generated Task ID: " + prototypeScopedBean.generateTaskId());
+
 		return taskService.findAll();
 	}
 
