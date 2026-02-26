695cd23c897bd602e79fb87b65b0895caef5de82
Luka Pavlov <pavlovld2007@gmail.com>
2026-02-26 21:15:52 +0300

    adding getAppInfo in TaskStatisticsService

--- src/main/java/com/example/service/TaskStatisticsService.java
+++ src/main/java/com/example/service/TaskStatisticsService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.example.repository.TaskRepository;
 
@@ -9,6 +10,15 @@ public class TaskStatisticsService {
 
 	private final TaskRepository taskRepositoryInMemory;
 	private final TaskRepository taskRepositoryStub;
+
+	@Value("${app.name}")
+	private String appName;
+
+	@Value("${app.version}")
+	private String appVersion;
+
+	@Value("${app.port}")
+	private String serverPort;
 
 	public TaskStatisticsService(TaskRepository taskRepositoryInMemory,
 															 @Qualifier("repositoryStub") TaskRepository taskRepositoryStub) {
@@ -20,4 +30,9 @@ public class TaskStatisticsService {
 		return "Primary bean: " + taskRepositoryInMemory + "\n"
 			+ "Qualifier bean: " + taskRepositoryStub;
 	}
+
+	public String getAppInfo() {
+		return "App: " + appName + " " + appVersion +  " running on port: " + serverPort;
+	}
+
 }
