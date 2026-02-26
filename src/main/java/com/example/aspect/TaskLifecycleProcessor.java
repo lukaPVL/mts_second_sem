7e09626341f0457447df2906abe5f4402282a8c1
Luka Pavlov <pavlovld2007@gmail.com>
2026-02-26 19:58:30 +0300

    adding postProcess for TaskService and TaskRepository

---
+++ src/main/java/com/example/aspect/TaskLifecycleProcessor.java
@@ -0,0 +1,33 @@
+package com.example.aspect;
+
+import com.example.repository.TaskRepository;
+import com.example.service.TaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskLifecycleProcessor implements BeanPostProcessor {
+
+	private static final Logger log = LoggerFactory.getLogger(TaskLifecycleProcessor.class);
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		if (bean instanceof TaskService || bean instanceof TaskRepository) {
+			log.info("До инициализации бина: {} (класс: {})",
+				beanName, bean.getClass());
+		}
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		if (bean instanceof TaskService || bean instanceof TaskRepository) {
+			log.info("После инициализации бина: {} (класс: {})",
+				beanName, bean.getClass());
+		}
+		return bean;
+	}
+}
