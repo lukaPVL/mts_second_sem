4fc23687fb4de41f79a9dd6ac29d187d4499c7ff
Luka Pavlov <pavlovld2007@gmail.com>
2026-02-26 17:31:22 +0300

    adding bean (RestController)

---
+++ src/main/java/Controller.java
@@ -0,0 +1,39 @@
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class Controller {
+	private final TaskService taskService;
+
+	public Controller(TaskService taskService) {
+		this.taskService = taskService;
+	}
+
+	@GetMapping
+	public List<Task> getAllTasks() {
+		return taskService.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Task getTaskById(@PathVariable Long id) {
+		return taskService.findById(id);
+	}
+
+	@PostMapping
+	public Task createTask(@RequestBody Task task) {
+		return taskService.save(task);
+	}
+
+	@PutMapping("/{id}")
+	public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+		task.setId(id);
+		return taskService.update(task);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteTask(@PathVariable Long id) {
+		taskService.deleteById(id);
+	}
+}
